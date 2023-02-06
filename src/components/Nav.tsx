b8f71bad48ab519788e7529b2cc92680c53b347d
Thomas Erhel <thomas.erhel@gmail.com>
2023-02-06 13:42:15 +0100

    Add little lemon colors on Nav and Hero

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,33 +1,34 @@
 import React from "react";
+import "../index.css";
 import { TopAppBar, TopAppBarRow, TopAppBarSection, TopAppBarTitle, TopAppBarFixedAdjust, TopAppBarActionItem, Tooltip } from "rmwc";
 
 
 export default function Nav() {
     return (
         <nav>
-            <TopAppBar>
+            <TopAppBar style={{ backgroundColor: '#FFFFFF' }}>
                 <TopAppBarRow>
                 <TopAppBarSection alignStart>
-                    <TopAppBarTitle>LITTLE LEMON</TopAppBarTitle>
+                    <TopAppBarTitle style={{ color: '#495E57' }}>LITTLE LEMON</TopAppBarTitle>
                 </TopAppBarSection>
                 <TopAppBarSection alignEnd>
                     <Tooltip content="Home">
-                    <TopAppBarActionItem icon="home" />
+                    <TopAppBarActionItem icon="home" style={{ color: '#495E57' }} />
                     </Tooltip>
                     <Tooltip content="About">
-                    <TopAppBarActionItem icon="info" />
+                    <TopAppBarActionItem icon="info" style={{ color: '#495E57' }} />
                     </Tooltip>
                     <Tooltip content="Menu">
-                    <TopAppBarActionItem icon="restaurant_menu" />
+                    <TopAppBarActionItem icon="restaurant_menu" style={{ color: '#495E57' }} />
                     </Tooltip>
                     <Tooltip content="Reservations">
-                    <TopAppBarActionItem icon="calendar_month" />
+                    <TopAppBarActionItem icon="calendar_month" style={{ color: '#495E57' }} />
                     </Tooltip>
                     <Tooltip content="Order online">
-                    <TopAppBarActionItem icon="shopping_basket" />
+                    <TopAppBarActionItem icon="shopping_basket" style={{ color: '#495E57' }} />
                     </Tooltip>
                     <Tooltip content="Login">
-                    <TopAppBarActionItem icon="login" />
+                    <TopAppBarActionItem icon="login" style={{ color: '#495E57' }} />
                     </Tooltip>
                 </TopAppBarSection>
                 </TopAppBarRow>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Hero from "../components/Hero";
+import Nav from "../components/Nav";
+
+
+export default function Home() {
+    return (
+        <>
+        <Nav />
+        <Hero />
+        </>
+    );
+}

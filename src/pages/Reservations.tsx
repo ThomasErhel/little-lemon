51b3961cb64915fc800e4927906510bc18596861
Thomas Erhel <thomas.erhel@gmail.com>
2023-02-07 20:35:43 +0100

    Update Reservations

--- src/pages/Reservations.tsx
+++ src/pages/Reservations.tsx
@@ -2,7 +2,6 @@ import BookingForm from "../components/BookingForm";
 import { useReducer } from "react";
 import { fetchAPI } from "../components/Api";
 import { Typography } from "rmwc";
-import { BrowserRouter } from "react-router-dom";
 
 export default function BookingPage() {
   function updateTimes(date: any) {
@@ -18,9 +17,7 @@ export default function BookingPage() {
         <Typography className="title" use="headline2" tag="h1">
             Reservations
         </Typography>
-        <BrowserRouter>
             <BookingForm availableTimes={availableTimes} updateTimes={dispatch} />
-        </BrowserRouter>
     </div>
   );
 }

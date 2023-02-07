3e69a88935533d59a141942a0ff9648b8cec0adc
Thomas Erhel <thomas.erhel@gmail.com>
2023-02-07 08:30:06 +0100

    Update BookingForm component and Reservations page

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -1,60 +1,70 @@
 import React from "react";
 import { Button, FormField, Select, Slider, TextField, Typography } from "rmwc";
 
-export default function BookingForm() {
-    const [selectedDate, setDate] = React.useState("");
-    const [selectedTime, setTime] = React.useState("");
-    const [guests, setGuests] = React.useState(1);
-    const [occasion, setOccasion] = React.useState("");
+interface Props {
+  availableTimes: {
+    selectedDate: string;
+    times: string[];
+  };
+  dispatch: (action: { type: string; date: string }) => void;
+}
 
-    const handleSubmit = (event: { preventDefault: () => void; }) => {
-        event.preventDefault();
-        console.log("Date", selectedDate, "Time:", selectedTime, "Guests:", guests, "Occasion:", occasion);
-    };
-    return (
-        <>
-        <Typography use="headline2" tag="h1">
-            Reservations
-        </Typography>
-        <form onSubmit={handleSubmit}>
-            <FormField style={{ display: 'grid', maxWidth: '200px', gap: '20px' }}>
-                <TextField
-                    label="date"
-                    type="date"
-                    min={new Date().toISOString().slice(0, 10)}
-                    required
-                    onChange={(event: { target: { value: React.SetStateAction<string>; }; }) => setDate(event.target.value)}
-                />
-                <Select
-                    label="Choose time"
-                    options={['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']}
-                    required
-                    onChange={(event: { target: { value: React.SetStateAction<string>; }; }) => setTime(event.target.value)}
-                />
-                <label htmlFor="guests">Number of guests</label>
-                <Slider
-                    value={guests}
-                    onChange={(evt: { detail: { value: React.SetStateAction<number>; }; }) => setGuests(evt.detail.value)}
-                    onInput={(evt: { detail: { value: React.SetStateAction<number>; }; }) => setGuests(evt.detail.value)}
-                    discrete
-                    step={1}
-                    min={1}
-                    max={10}
-                    id="guests"
-                />
-                <Select
-                    label="Occasion"
-                    options={['Birthday', 'Anniversary']}
-                    onChange={(event: { target: { value: React.SetStateAction<string>; }; }) => setOccasion(event.target.value)}
-                />
-                <Button
-                    label="Make Your reservation"
-                    raised
-                    type="submit"
-                    style={{ backgroundColor: '#F4CE14', color: '#000000' }}
-                />
-            </FormField>
-        </form>
-        </>
-    );
+export default function BookingForm(props: Props) {
+  const [selectedTime, setTime] = React.useState("");
+  const [guests, setGuests] = React.useState(1);
+  const [occasion, setOccasion] = React.useState("");
+
+  const handleSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    console.log("Date", props.availableTimes.selectedDate, "Time:", selectedTime, "Guests:", guests, "Occasion:", occasion);
+  };
+  return (
+    <>
+    <Typography use="headline2" tag="h1">
+        Reservations
+    </Typography>
+    <form onSubmit={handleSubmit}>
+        <FormField style={{ display: 'grid', maxWidth: '200px', gap: '20px' }}>
+            <TextField
+                label="date"
+                type="date"
+                min={new Date().toISOString().slice(0, 10)}
+                required
+                value={props.availableTimes.selectedDate}
+                onChange={(event: { target: { value: string; }; }) => {
+                  props.dispatch({ type: "updateTimes", date: event.target.value });
+                }}
+            />
+            <Select
+                label="Choose time"
+                options={props.availableTimes.times}
+                required
+                onChange={(event: { target: { value: React.SetStateAction<string>; }; }) => setTime(event.target.value)}
+            />
+            <label htmlFor="guests">Number of guests</label>
+            <Slider
+                value={guests}
+                onChange={(evt: { detail: { value: React.SetStateAction<number>; }; }) => setGuests(evt.detail.value)}
+                onInput={(evt: { detail: { value: React.SetStateAction<number>; }; }) => setGuests(evt.detail.value)}
+                discrete
+                step={1}
+                min={1}
+                max={10}
+                id="guests"
+            />
+            <Select
+                label="Occasion"
+                options={['Birthday', 'Anniversary']}
+                onChange={(event: { target: { value: React.SetStateAction<string>; }; }) => setOccasion(event.target.value)}
+            />
+            <Button
+                label="Make Your reservation"
+                raised
+                type="submit"
+                style={{ backgroundColor: '#F4CE14', color: '#000000' }}
+            />
+        </FormField>
+    </form>
+    </>
+  );
 }
--- src/pages/Reservations.tsx
+++ src/pages/Reservations.tsx
@@ -1,11 +1,36 @@
-import React from "react";
-import { Grid, GridCell } from "rmwc";
+import React, { useReducer } from 'react';
 import BookingForm from "../components/BookingForm";
 
+interface AvailableTimesState {
+  selectedDate: string;
+  times: string[];
+}
+
+const initialState: AvailableTimesState = {
+  selectedDate: new Date().toISOString().slice(0, 10),
+  times: ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'],
+};
+
+const updateTimes = (state: AvailableTimesState, action: { type: string; date: string }) => {
+  switch (action.type) {
+    case "updateTimes":
+      return {
+        ...state,
+        selectedDate: action.date,
+        times: ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'],
+      };
+    default:
+      return state;
+  }
+};
+
+const initializeTimes = () => initialState;
+
 export default function Reservations() {
-    return (
-        <div className="App-reservations">
-            <BookingForm />
-        </div>
-    );
+  const [state, dispatch] = useReducer(updateTimes, initialState, initializeTimes);
+  return (
+    <div className="App-reservations">
+      <BookingForm availableTimes={state} dispatch={dispatch} />
+    </div>
+  );
 }

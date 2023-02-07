848a1e895ba9549b642bc1e7caa4f2af434b5199
Thomas Erhel <thomas.erhel@gmail.com>
2023-02-07 08:43:52 +0100

    Add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,25 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import BookingForm from './components/BookingForm';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Reservations from "./pages/Reservations";
 
+describe("BookingForm", () => {
+  test("renders heading text", () => {
+    render(<Reservations />);
+    const heading = screen.getByText("Reservations");
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("renders label text", () => {
+    render(<Reservations />);
+    const label = screen.getByText("Number of guests");
+    expect(label).toBeInTheDocument();
+  });
+});
+
+describe("BookingForm component", () => {
+    it("renders the 'Make Your reservation' button", () => {
+      render(<Reservations />);
+      const button = screen.getByText("Make Your reservation");
+      expect(button).toBeInTheDocument();
+    });
+  });

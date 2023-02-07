784eab5edab4c5e415f8839eb2922000be5c8aa9
Thomas Erhel <thomas.erhel@gmail.com>
2023-02-07 21:01:29 +0100

    Update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -23,3 +23,33 @@ describe("BookingForm component", () => {
       expect(button).toBeInTheDocument();
     });
   });
+
+  describe('initializeTimes', () => {
+    it('calls fetchAPI and dispatches an action with the returned times', async () => {
+      const mockDispatch = jest.fn();
+      const mockFetchAPI = jest.fn(() => Promise.resolve(['17:00 PM', '19:00 PM', '21:00 PM']));
+
+      await initializeTimes()(mockDispatch, undefined, { fetchAPI: mockFetchAPI });
+
+      expect(mockFetchAPI).toHaveBeenCalled();
+      expect(mockDispatch).toHaveBeenCalledWith({
+        type: INITIALIZE_TIMES,
+        times: ['17:00 PM', '19:00 PM', '21:00 PM'],
+      });
+    });
+  });
+
+  describe('updateTimes', () => {
+    it('calls fetchAPI and dispatches an action with the returned times', async () => {
+      const mockDispatch = jest.fn();
+      const mockFetchAPI = jest.fn(() => Promise.resolve(['17:00 PM', '19:00 PM', '21:00 PM']));
+
+      await updateTimes('2022-03-01')(mockDispatch, undefined, { fetchAPI: mockFetchAPI });
+
+      expect(mockFetchAPI).toHaveBeenCalledWith('2023-03-01');
+      expect(mockDispatch).toHaveBeenCalledWith({
+        type: UPDATE_TIMES,
+        times: ['17:00 PM', '19:00 PM', '21:00 PM'],
+      });
+    });
+  });
